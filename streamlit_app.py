8d3cc2d6718998b832c5d08912d73ff7574afe31
aidnmartn <99107214+aidnmartn@users.noreply.github.com>
2022-04-17 16:18:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+from sys import addaudithook
 import streamlit as st
 from PIL import Image
 from PIL import ImageDraw
@@ -5,18 +6,22 @@ from PIL import ImageFont
 import pandas as pd
 from io import BytesIO
 from pyxlsb import open_workbook as open_xlsb
+from datetime import datetime
 title = Image.open('title.png')
 icon = Image.open('icon1.png')
 layout = Image.open('HangarLayout.png')
-st.set_page_config(page_title="Hangars", page_icon=icon, layout="wide", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Hangars", page_icon=icon, layout="wide", initial_sidebar_state="collapsed", menu_items=None)
 title1, title2, title3 = st.columns(3)
 title1.image(title, use_column_width='always')
 st.title("JGG Hangar Dashboard")
-st.image(layout)
 #hangarColumns = ['Tenant', 'Tail Number', 'Type']
 #indices = ['A', 'B', 'C', 'D', '1A', '2B', '3C', '4D', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '38', '39', '40', '41', '42', '43', '44', '45', '46', '46A', '47', '48', '49', '50', '51', '52', '53', '54', '55', '55A']
 #hangarData = pd.DataFrame(columns=hangarColumns, index=indices)
 
+#coordColumns = ['x', 'y']
+#coordIndices = ['A', 'B', 'C', 'D', '1A', '2B', '3C', '4D', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '38', '39', '40', '41', '42', '43', '44', '45', '46', '46A', '47', '48', '49', '50', '51', '52', '53', '54', '55', '55A']
+#coords = pd.DataFrame(columns=coordColumns, index=coordIndices)
+coords = pd.read_csv('coords.csv', index_col=0)
 def to_excel(df):
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
@@ -30,36 +35,44 @@ def to_excel(df):
     return processed_data
 
 
-def updateImage(xcor, ycor):
-    edit = ImageDraw.Draw(layout)
-    edit.text((xcor, ycor), "test", fill=(0, 0, 0))
-    layout.save("HangarLayout.png")
+def updateImage(xy, data):
+    temp=layout.copy()
+    myFont = ImageFont.truetype('times new roman.ttf', 20)
+    edit = ImageDraw.Draw(temp)
+    for i in data.index:
+        text = data.at[i,'Tenant'] + '\n' + data.at[i,'Tail Number'] + '\n' + data.at[i,'Type']
+        edit.text((xy.at[i,'x'], xy.at[i,'y']), text, font=myFont, fill=(0, 0, 0))
+    edit.text((1450, 650), "46A:", font=myFont, fill=(0, 0, 0))
+    edit.text((100, 630), "55A:", font=myFont, fill=(0, 0, 0))
+    temp.save("temp.png")
+    filled = Image.open("temp.png")
+    container.image(filled)
 
-
-
-col1, col2 = st.columns(2)
 hangarData = pd.read_pickle('HangarData.pkl') 
-form = col1.form("HangarEdit")
-form.write("Use this form to update the tenant, tail number, and type of aircraft in each hangar. To clear a hangar, enter in the Hangar ID and leave the rest of the fields blank. Click the sidebar on the left for additional tools.")
-hangarId = form.text_input("Enter the hangar code of the hangar you would like to modify:")
-tenant = form.text_input("Enter the name of the new tenant:")
+container = st.container()
+form = st.form("HangarEdit")
+expander = st.expander('View Data')
+form.write("Use this form to update the *tenant*, *tail number*, and *type of aircraft* in each hangar.")
+form.write('**To clear a hangar**, enter in the Hangar ID, leave the rest of the fields blank, and click "Update Hangar Info"')
+form.write('**To display the hangar chart and data table** without changing data, leave all fields blank and click "Update Hangar Info"')
+form.write("Click the sidebar on the left for additional tools.")
+form.write("*Note: Longer names may need to be abbreviated to fit*")
+hangarId = form.text_input("Enter the Hangar ID of the hangar you would like to modify:")
+tenant = form.text_input("Enter the last name of the new tenant:")
 tail = form.text_input("Enter the tail number of the aircraft:")
 type = form.text_input("Enter the type of aircraft:")
-
-
-
-
-if form.form_submit_button("Update Hangar info"):
+if form.form_submit_button("Update Hangar Info"):
     if hangarId in hangarData.index:
+
         st.success(f"Hangar {hangarId} Updated")
         hangarData.at[hangarId, 'Tenant'] = tenant
         hangarData.at[hangarId, 'Tail Number'] = tail
         hangarData.at[hangarId, 'Type'] = type
         hangarData.to_pickle('HangarData.pkl')
-        col2.write(hangarData)
     else:
         st.error("Enter a valid Hangar ID")
-
+    expander.write(hangarData)
+    updateImage(coords,hangarData)
 
 
 
@@ -67,5 +80,8 @@ if form.form_submit_button("Update Hangar info"):
 with st.sidebar:
     st.subheader("Additional tools")
     df_xlsx = to_excel(hangarData)
-    st.download_button(label='Download hangar data as Excel file', data=df_xlsx , file_name= 'HangarData.xlsx')
-  
+    now = datetime.now()
+    dt_string = now.strftime("%d-%m-%Y_%H.%M.%S")
+    st.download_button(label='Download hangar data as Excel file', data=df_xlsx , file_name= f'HangarData_{dt_string}.xlsx')
+
+#coords.to_csv('coords.csv')
