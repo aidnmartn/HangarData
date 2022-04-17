33258c24464c861a7be3f451bf1b364c4ec4bad7
aidnmartn <99107214+aidnmartn@users.noreply.github.com>
2022-04-17 17:32:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,13 @@ from datetime import datetime
 title = Image.open('title.png')
 icon = Image.open('icon1.png')
 layout = Image.open('HangarLayout.png')
-st.set_page_config(page_title="Hangars", page_icon=icon, layout="centered", initial_sidebar_state="collapsed", menu_items=None)
+st.set_page_config(page_title="Hangars", page_icon=icon, layout="centered", initial_sidebar_state="expanded", menu_items=None)
 title1, title2, title3 = st.columns(3)
 title1.image(title, use_column_width='always')
 st.title("JGG Hangar Dashboard")
 #hangarColumns = ['Tenant', 'Tail Number', 'Type']
-#indices = ['A', 'B', 'C', 'D', '1A', '2B', '3C', '4D', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '38', '39', '40', '41', '42', '43', '44', '45', '46', '46A', '47', '48', '49', '50', '51', '52', '53', '54', '55', '55A']
-#hangarData = pd.DataFrame(columns=hangarColumns, index=indices)
+ids = ['A', 'B', 'C', '1A', '2B', '3C', '4D', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '38', '39', '40', '41', '42', '43', '44', '45', '46', '46A', '47', '48', '49', '50', '51', '52', '53', '54', '55', '55A']
+#hangarData = pd.DataFrame(columns=hangarColumns, index=ids)
 
 #coordColumns = ['x', 'y']
 #coordIndices = ['A', 'B', 'C', 'D', '1A', '2B', '3C', '4D', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '38', '39', '40', '41', '42', '43', '44', '45', '46', '46A', '47', '48', '49', '50', '51', '52', '53', '54', '55', '55A']
@@ -50,14 +50,18 @@ def updateImage(xy, data):
 
 hangarData = pd.read_pickle('HangarData.pkl') 
 container = st.container()
+updateImage(coords,hangarData)
+help = st.expander('Help')
 form = st.form("HangarEdit")
-expander = st.expander('View Data')
-form.write("Use this form to update the *tenant*, *tail number*, and *type of aircraft* in each hangar.")
-form.write('**To clear a hangar**, enter in the Hangar ID, leave the rest of the fields blank, and click "Update Hangar Info"')
-form.write('**To display the hangar chart and data table** without changing data, leave all fields blank and click "Update Hangar Info"')
-form.write("Click the sidebar on the left for additional tools.")
-form.write("*Note: Longer names may need to be abbreviated to fit*")
-hangarId = form.text_input("Enter the Hangar ID of the hangar you would like to modify:")
+st.write(hangarData)
+help.write("Use this form to update the *tenant*, *tail number*, and *type of aircraft* in each hangar.")
+help.write('**To clear a hangar**, enter in the Hangar ID, leave the rest of the fields blank, and click "Update Hangar Info"')
+help.write('**To display the hangar chart and data table** without changing data, leave all fields blank and click "Update Hangar Info"')
+help.write("Click the sidebar on the left for additional tools.")
+help.write("You can right-click on the hangar chart to save to your computer")
+help.write("The data table at the bottom is interactive, for example: click the 'Tenant' column to sort alphabetically by tenant name")
+help.write("*Note: Longer names may need to be abbreviated to fit*")
+hangarId = form.selectbox("Choose the Hangar ID of the hangar you would like to modify:", ids)
 tenant = form.text_input("Enter the last name of the new tenant:")
 tail = form.text_input("Enter the tail number of the aircraft:")
 type = form.text_input("Enter the type of aircraft:")
